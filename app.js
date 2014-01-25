c9e3010b054db78c8daef52ff55f008c3de90fc8
nikita <nikita-bragin@mail.ru>
2014-01-25 15:22:26 +0400

    Remove cluster

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 'use strict';
 
-var cluster = require('cluster'),
-    os = require('os'),
-    cpuCount = os.cpus().length / 2;
+//Импортируем глобальный объект soshace
+require('./app/src/config');
 
-cluster.setupMaster({
-    exec: './app/src/worker.js'
-});
+var Express = require('express'),
+    Package = require('./package'),
+    App = new Express(),
+    DbConnection = require('./app/src/common/dbConnection').DbConnection,
+    Router = require('./app/src/common/router').Router;
 
-//Запускаем процессы по количеству ядер процессора
-for (var i = 0; i < cpuCount; i += 1) {
-    cluster.fork();
-}
+var Blog = {
+    /**
+     * Инициализируем приложение
+     *
+     * @private
+     * @function
+     * @name Worker.init
+     * @return {undefined}
+     */
+    _init: function () {
+        soshace.VERSION = Package.version;
+        soshace.ENVIRONMENT = App.get('env');
+        this._configure();
+        //Подрубаемся к базе
+        DbConnection.databaseOpen(function () {
+            Router.init(App);
+            App.listen(soshace.PORT, soshace.HOST);
+        });
 
-cluster.on('exit', function () {
-    cluster.fork();
-});
+    },
+
+    /**
+     * Конфигурируем наше приложение
+     *
+     * @private
+     * @function
+     * @name Worker.configure
+     * @return {undefined}
+     */
+    _configure: function () {
+        App.configure(function () {
+            App.use(Express.bodyParser());
+            App.set('views', 'app/src/views');
+            App.set('view engine', 'jade');
+        });
+    }
+};
+
+Blog._init();
