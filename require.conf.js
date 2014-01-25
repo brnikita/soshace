e11870e4ea8a52fa9a5d6d1b5b735940f7979076
nikita <nikita-bragin@mail.ru>
2014-01-25 18:20:29 +0400

    Load modules by data attribute

--- require.conf.js
+++ require.conf.js
@@ -2,7 +2,9 @@ require.config(
     {
         baseUrl: '/static/scripts/',
         paths: {
+            'utils': 'utils',
             'jquery': 'libs/jquery',
+            'underscore': 'libs/underscore',
             'bootstrap': 'libs/bootstrap',
             'prettify': 'libs/prettify'
         },
