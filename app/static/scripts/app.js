4e7b9fbdb5c78eaee65c3e7e8ded568f9c42bd8c
nikita <nikita-bragin@mail.ru>
2014-01-25 18:13:46 +0400

    Load modules by data attribute

--- app/static/scripts/app.js
+++ app/static/scripts/app.js
@@ -1,7 +1,51 @@
 'use strict';
 
-require(['prettify'], function () {
-    window.prettyPrint();
+require([
+    'jquery',
+    'underscore',
+    'modules/postPreviewModule',
+    'modules/postDetailModule'
+], function ($) {
+    var Blog = {
+
+        /**
+         * @method
+         * @name Blog.initialize
+         * @returns {undefined}
+         */
+        initialize: function () {
+            var _this = this;
+
+            $(function () {
+                $('.js-module').each(_this.loadModule);
+            });
+        },
+
+        /**
+         * Инициализирует модули
+         * по имени в атрибуте 'data-module'
+         *
+         * @method
+         * @name Blog.loadModule
+         * @returns {undefined}
+         */
+        loadModule: function () {
+            var $this = $(this),
+                elementData = $this.data(),
+                modulePath;
+
+            if (elementData.module) {
+                modulePath = 'modules/' + elementData.module;
+                require([modulePath], function (module) {
+                    module.initialize({
+                        context: $this
+                    });
+                });
+            }
+        }
+    };
+
+    Blog.initialize();
 });
 
 
