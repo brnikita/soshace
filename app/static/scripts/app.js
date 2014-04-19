3998d366ae9a7206101cc8ec8c19fab4dd4bf971
nikita <nikita-bragin@mail.ru>
2014-04-20 02:55:25 +0400

    Начал добавление backbone на клиент

--- app/static/scripts/app.js
+++ app/static/scripts/app.js
@@ -57,110 +57,14 @@ require([
         loadModule: function ($el) {
             var elementData = $el.data(),
                 moduleName = elementData.module,
-                //Заменяем слеши на точки
-                formattedModuleName = moduleName.replace(/\//g, '.'),
                 modulePath;
 
             if (elementData.module) {
                 modulePath = 'modules/' + moduleName;
-                require([modulePath], _.bind(function (module) {
-                    module.$el = $el;
-                    module.initialize();
-                    this._delegateEvents(module, module.events, $el, formattedModuleName);
+                require([modulePath], _.bind(function (Module) {
+                    new Module({$el: $el});
                 }, this));
             }
-        },
-
-        /**
-         * Берет список событий из events и навешивает на
-         * родительский элемент $el
-         *
-         * Метод выдран из Backbone и немного переписан)
-         *
-         *  Set callbacks, where `this.events` is a hash of
-         *
-         * *{"event selector": "callback"}*
-         *
-         *     {
-         *          'mousedown .title':  'edit',
-         *          'click .button':     'save',
-         *          'click .open':       function(e) { ... }
-         *      }
-
-         * pairs. Callbacks will be bound to the view, with `this` set properly.
-         * Uses event delegation for efficiency.
-         * Omitting the selector binds the event to `this.el`.
-         * This only works for delegate-able events: not `focus`, `blur`, and
-         * not `change`, `submit`, and `reset` in Internet Explorer.
-         *
-         * @private
-         * @method
-         * @name Blog._delegateEvents
-         * @param {Object} module интересующий модуль
-         * @param {Object} events список обработчиков событий
-         * @param {jQuery} $el корневой элемент модуля
-         * @param {string} moduleIndex идентификатор модуля (название модуля)
-         * @returns {undefined}
-         */
-        _delegateEvents: function (module, events, $el, moduleIndex) {
-            var key,
-                method,
-                match,
-                selector,
-                eventName,
-                delegateEventSplitter = /^(\S+)\s*(.*)$/;
-
-            if (typeof events !== 'object') {
-                return;
-            }
-
-            this._unDelegateEvents($el, moduleIndex);
-
-            for (key in events) {
-                if (events.hasOwnProperty(key)) {
-                    method = events[key];
-
-                    if (!_.isFunction(method)) {
-                        method = module[events[key]];
-                    }
-
-                    if (!method) {
-                        continue;
-                    }
-
-                    match = key.match(delegateEventSplitter);
-                    eventName = match[1];
-                    selector = match[2];
-                    method = _.bind(method, module);
-                    eventName += '.delegateEvents' + moduleIndex;
-
-                    if (selector === '') {
-                        $el.on(eventName, method);
-                    } else {
-                        $el.on(eventName, selector, method);
-                    }
-                }
-
-            }
-        },
-
-        /**
-         * Очищает все callback навешенные на родительский элемент DOM
-         * модуля. Удаляются только те слушатели, которые бы навешены самим модулем
-         * до этого.
-         *
-         * Clears all callbacks previously bound to the view with `delegateEvents`.
-         * You usually don't need to use this, but may wish to if you have multiple
-         * Backbone views attached to the same DOM element.
-         *
-         * @private
-         * @name Blog._unDelegateEvents
-         * @param {jQuery} $el корневой элемент модуля
-         * @param {string} moduleIndex идентификатор модуля (название модуля)
-         * @returns {undefined}
-         */
-        _unDelegateEvents: function ($el, moduleIndex) {
-            $el.off('.delegateEvents' + moduleIndex);
         }
     };
 
