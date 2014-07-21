335108198d6667632e43cc842935a8c5209b4e4f
brnikita <nikita-bragin@mail.ru>
2014-07-22 02:18:57 +0400

    Начал переход на express4

--- app.js
+++ app.js
@@ -4,15 +4,17 @@
 require('./app/config');
 
 var _ = require('underscore'),
-    Express = require('express'),
+    express = require('express'),
     Package = require('./package'),
-    App = new Express(),
+//    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    App = express(),
     DbConnection = require('./app/src/common/dbConnection'),
     I18n = require('i18n-2'),
     Router = require('./app/src/router'),
     Passport = require('passport'),
-    Strategies = require('./app/src/common/strategies'),
-    Handlebars = require('express3-handlebars');
+    Strategies = require('./app/src/common/strategies');
 
 var Blog = {
     /**
@@ -45,15 +47,15 @@ var Blog = {
      * @return {undefined}
      */
     configure: function () {
-        App.use(Express.bodyParser());
+        App.use(cookieParser());
         App.enable('view cache');
         App.set('views', 'app/views/');
-        App.engine('hbs', new Handlebars({
-            layoutsDir: 'app/views/layouts',
-            partialsDir: 'app/views/partials',
-            defaultLayout: 'layoutView',
-            extname: '.hbs'
-        }));
+//        App.engine('hbs', new Handlebars({
+//            layoutsDir: 'app/views/layouts',
+//            partialsDir: 'app/views/partials',
+//            defaultLayout: 'layoutView',
+//            extname: '.hbs'
+//        }));
 
         App.set('view engine', 'hbs');
         I18n.expressBind(App, {
@@ -63,13 +65,15 @@ var Blog = {
             defaultLocale: Soshace.DEFAULT_LOCALE
         });
 
-        App.use(Express.cookieParser());
-        App.use(Express.bodyParser());
-        App.use(Express.methodOverride());
-        App.use(Express.session({ secret: Soshace.SESSION_KEY }));
+        //http://stackoverflow.com/a/24330353
+//        App.use(bodyParser());
+        App.use(session({
+            secret: Soshace.SESSION_KEY,
+            saveUninitialized: true,
+            resave: true
+        }));
         App.use(Passport.initialize());
         App.use(Passport.session());
-        App.use(App.router);
 
         //Устанавливаем ответ для 404
         App.use(this.notFoundHandler);
