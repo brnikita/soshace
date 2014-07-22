437d04a2e8cc414c9d352e9d9431947062569f58
Nikita Bragin <bragin.na@vuaro.ru>
2014-07-22 12:05:20 +0400

    Перевел проект на Express 4

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('./app/config');
 var _ = require('underscore'),
     express = require('express'),
     Package = require('./package'),
-//    bodyParser = require('body-parser'),
+    bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
     App = express(),
@@ -14,9 +14,12 @@ var _ = require('underscore'),
     I18n = require('i18n-2'),
     Router = require('./app/src/router'),
     Passport = require('passport'),
-    Strategies = require('./app/src/common/strategies');
+    Handlebars = require('express3-handlebars'),
+    Strategies = require('./app/src/common/strategies'),
+    Class = require('./app/src/vendors/class'),
+    methodOverride = require('method-override');
 
-var Blog = {
+var Blog = Class.extend({
     /**
      * Инициализируем приложение
      *
@@ -47,15 +50,17 @@ var Blog = {
      * @return {undefined}
      */
     configure: function () {
+        App.use(bodyParser.json());
         App.use(cookieParser());
+        App.use(methodOverride());
         App.enable('view cache');
         App.set('views', 'app/views/');
-//        App.engine('hbs', new Handlebars({
-//            layoutsDir: 'app/views/layouts',
-//            partialsDir: 'app/views/partials',
-//            defaultLayout: 'layoutView',
-//            extname: '.hbs'
-//        }));
+        App.engine('hbs', new Handlebars({
+            layoutsDir: 'app/views/layouts',
+            partialsDir: 'app/views/partials',
+            defaultLayout: 'layoutView',
+            extname: '.hbs'
+        }));
 
         App.set('view engine', 'hbs');
         I18n.expressBind(App, {
@@ -65,8 +70,6 @@ var Blog = {
             defaultLocale: Soshace.DEFAULT_LOCALE
         });
 
-        //http://stackoverflow.com/a/24330353
-//        App.use(bodyParser());
         App.use(session({
             secret: Soshace.SESSION_KEY,
             saveUninitialized: true,
@@ -74,38 +77,7 @@ var Blog = {
         }));
         App.use(Passport.initialize());
         App.use(Passport.session());
-
-        //Устанавливаем ответ для 404
-        App.use(this.notFoundHandler);
-    },
-
-    /**
-     * Обработчик ошибки 404
-     *
-     * @method
-     * @name Blog.notFoundHandler
-     * @param {Object} request
-     * @param {Object} response
-     * @returns {undefined}
-     */
-    notFoundHandler: function (request, response) {
-        response.status(404);
-
-        // respond with html page
-        if (request.accepts('html')) {
-            response.render('404', { url: request.url });
-            return;
-        }
-
-        // respond with json
-        if (request.accepts('json')) {
-            response.send({ error: 'Not found' });
-            return;
-        }
-
-        // default to plain-text. send()
-        response.type('txt').send('Not found');
     }
-};
+});
 
-Blog.initialize();
+void new Blog();
