04a86a0ec061b41e1dadd0b61a970019ff77d96e
Nikita Bragin <bragin.na@vuaro.ru>
2014-09-03 13:47:30 +0400

    Убрал фоны

--- app/src/common/requestParams.js
+++ app/src/common/requestParams.js
@@ -10,25 +10,10 @@
  * @returns {Object}
  */
 module.exports = function (request) {
-    var _ = require('underscore'),
-        HandlebarsHelpers = require('./handlebarsHelpers'),
+    var HandlebarsHelpers = require('./handlebarsHelpers'),
         helpers = new HandlebarsHelpers(request),
         profile = request.user && request.user[0],
         profileUserName = profile && profile.userName;
-
-    /**
-     * Метод возвращает клсасс фона рандомный
-     *
-     * @method
-     * @returns {String} класс фона
-     */
-    function getRandomBackground(){
-        var backgrounds = Soshace.BACKGROUNDS,
-            max = backgrounds.length - 1,
-            random = _.random(0, max);
-
-        return backgrounds[random];
-    }
 
     return {
         isProduction: Soshace.IS_PRODUCTION,
@@ -39,7 +24,6 @@ module.exports = function (request) {
         paths: Soshace.PATHS,
         profile: profile,
         profileUserName: profileUserName,
-        bodyBg: getRandomBackground(),
         //для шаблонов
         backend: true
     };
