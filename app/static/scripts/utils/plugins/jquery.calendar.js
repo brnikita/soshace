0645c3ac4d87838477f4b12c224af61cf1c10e0e
brnikita <nikita-bragin@mail.ru>
2014-09-18 01:40:10 +0400

    Плагин календаря

--- app/static/scripts/utils/plugins/jquery.calendar.js
+++ app/static/scripts/utils/plugins/jquery.calendar.js
@@ -5,22 +5,104 @@
  */
 define([
     'jquery',
-    'underscore',
-    'utils/helpers'
-], function ($, _, Helpers) {
-    var methods = {
-        /**
-         * @constructor
-         * @param {Object} options
-         * @returns {undefined}
-         */
-        initialize: function (options) {
-            return this.each(function () {
-                var $this = $(this);
+    'underscore'
+], function ($, _) {
+    var pluginOptions = {
+            startYear: 1900
+        },
+        methods = {
+            /**
+             * @constructor
+             * @name jQuery.fn.masks#initialize
+             * @param {Object} options
+             *                 [options.startYear] - год, с которого начинается календарь
+             *                 [options.endYear] - год, на котором заканчивается календарь
+             *                 options.dateField - имя поля даты
+             *                 options.monthField - имя поля месяца
+             *                 options.fullYearField - имя поля года
+             * @returns {undefined}
+             */
+            initialize: function (options) {
+                pluginOptions = _.extend(pluginOptions, options);
 
-            });
-        }
-    };
+                return this.each(_.bind(function () {
+                    var lists = this.getLists(),
+                        $this = $(this),
+                        template = Soshace.hbs['plugins/jquery/calendar/calendar'](
+                            _.extend(lists, pluginOptions));
+
+                    $this.html(template);
+                }, this));
+            },
+
+            /**
+             * Метод возвращает списки дат, месяцев и лет
+             *
+             * @method
+             * @name jQuery.fn.masks#getLists
+             * @returns {Object}
+             */
+            getLists: function () {
+                var _this = this;
+
+                return {
+                    datesList: _this.getDatesList(),
+                    monthsList: _this.getMothsList(),
+                    fullYearsList: _this.getYearsList()
+                };
+            },
+
+            /**
+             * Метод возвращает список лет
+             *
+             * @method
+             * @name jQuery.fn.masks#getYearsList
+             * @returns {Array}
+             */
+            getYearsList: function () {
+                var currentDate = new Date(),
+                    currentYear = currentDate.getFullYear(),
+                    startYear = pluginOptions.startYear,
+                    endYear = pluginOptions.endYear,
+                    isCurrentYear,
+                    year,
+                    list = [];
+
+                endYear = endYear || currentYear;
+
+                for (year = startYear; year < endYear; year++) {
+                    isCurrentYear
+                    list.push({
+                        value: year,
+                        title: year,
+                        selected: year === currentYear
+                    });
+                }
+                return list;
+            },
+
+            /**
+             * Метод возвращает список месяцев
+             *
+             * @method
+             * @name jQuery.fn.masks#getMothsList
+             * @returns {undefined}
+             */
+            getMothsList: function () {
+
+            },
+
+            /**
+             * Метод возвращает список дат
+             *
+             * @method
+             * @name jQuery.fn.masks#getDatesList
+             * @returns {undefined}
+             */
+            getDatesList: function () {
+
+            }
+        };
 
     $.fn.masks = function (method) {
         if (methods[method]) {
