9aaf5733ec2537c4723c1923b5bc231891196a39
Nikita Bragin <bragin.na@vuaro.ru>
2014-09-18 20:04:38 +0400

    Плагин календаря

--- app/static/scripts/utils/plugins/jquery.calendar.js
+++ app/static/scripts/utils/plugins/jquery.calendar.js
@@ -7,39 +7,127 @@ define([
     'jquery',
     'underscore'
 ], function ($, _) {
-    var pluginOptions = {
+    var defaultOptions = {
             startYear: 1900
         },
+        monthsList = [
+            {
+                value: 1,
+                title: 'january'
+            },
+            {
+                value: 2,
+                title: 'february'
+            },
+            {
+                value: 3,
+                title: 'march'
+            },
+            {
+                value: 4,
+                title: 'april'
+            },
+            {
+                value: 5,
+                title: 'may'
+            },
+            {
+                value: 6,
+                title: 'june'
+            },
+            {
+                value: 7,
+                title: 'july'
+            },
+            {
+                value: 8,
+                title: 'august'
+            },
+            {
+                value: 9,
+                title: 'september'
+            },
+            {
+                value: 10,
+                title: 'october'
+            },
+            {
+                value: 11,
+                title: 'november'
+            },
+            {
+                value: 12,
+                title: 'december'
+            }
+
+        ],
         methods = {
             /**
              * @constructor
-             * @name jQuery.fn.masks#initialize
+             * @name jQuery.fn.calendar#initialize
              * @param {Object} options
              *                 [options.startYear] - год, с которого начинается календарь
              *                 [options.endYear] - год, на котором заканчивается календарь
+             *                 options.locale - локаль календаря
+             *                 options.selectedDate - выставленная дата в календаре
              *                 options.dateField - имя поля даты
              *                 options.monthField - имя поля месяца
              *                 options.fullYearField - имя поля года
              * @returns {undefined}
              */
             initialize: function (options) {
-                pluginOptions = _.extend(pluginOptions, options);
+                var pluginOptions = _.extend(defaultOptions, options);
 
                 return this.each(_.bind(function () {
-                    var lists = this.getLists(),
-                        $this = $(this),
-                        template = Soshace.hbs['plugins/jquery/calendar/calendar'](
-                            _.extend(lists, pluginOptions));
-
-                    $this.html(template);
+                    this.setOptions(pluginOptions);
                 }, this));
+            },
+
+            /**
+             * Метод рендерит календари на основе параметров
+             *
+             * @method
+             * @name jQuery.fn.calendar#render
+             * @returns {undefined}
+             */
+            render: function () {
+                var lists = this.getLists(),
+                    options = this.getOptions(),
+                    template = Soshace.hbs['plugins/jquery/calendar/calendar'](
+                        _.extend(lists, options));
+
+                this.html(template);
+            },
+
+            /**
+             * Метод сохраняет настройки в данных элемента
+             *
+             * @method
+             * @name jQuery.fn.calendar#setOptions
+             * @param {Object} options
+             * @returns {undefined}
+             */
+            setOptions: function (options) {
+                var pluginOptions = this.getOptions();
+                this.data('calendar', _.extend(pluginOptions, options));
+            },
+
+            /**
+             * Метод возвращает параметры контрола
+             *
+             * @method
+             * @name jQuery.fn.calendar#getOptions
+             * @returns {Object}
+             */
+            getOptions: function () {
+                return this.data('calendar');
             },
 
             /**
              * Метод возвращает списки дат, месяцев и лет
              *
              * @method
-             * @name jQuery.fn.masks#getLists
+             * @name jQuery.fn.calendar#getLists
              * @returns {Object}
              */
             getLists: function () {
@@ -56,36 +144,60 @@ define([
              * Метод возвращает список лет
              *
              * @method
-             * @name jQuery.fn.masks#getYearsList
+             * @name jQuery.fn.calendar#getYearsList
              * @returns {Array}
              */
             getYearsList: function () {
                 var currentDate = new Date(),
                     currentYear = currentDate.getFullYear(),
-                    startYear = pluginOptions.startYear,
-                    endYear = pluginOptions.endYear,
-                    isCurrentYear,
+                    options = this.getOptions(),
+                    startYear = options.startYear,
+                    endYear = options.endYear,
+                    selectedYear = this.getSelectedYear(),
+                    isSelected,
                     year,
                     list = [];
 
                 endYear = endYear || currentYear;
 
                 for (year = startYear; year < endYear; year++) {
-                    isCurrentYear
+                    isSelected = year === selectedYear;
                     list.push({
                         value: year,
                         title: year,
-                        selected: year === currentYear
+                        selected: isSelected
                     });
                 }
                 return list;
             },
 
             /**
+             * Метод возвращает установленный год
+             *
+             * @method
+             * @name jQuery.fn.calendar#getSelectedYear
+             * @returns {Number}
+             */
+            getSelectedYear: function () {
+                var options = this.getOptions(),
+                    selectedDate = options.selectedDate,
+                    selectedDateObj,
+                    today;
+
+                if (_.isUndefined(selectedDate)) {
+                    today = new Date();
+                    return today.getFullYear();
+                }
+
+                selectedDateObj = new Date(selectedDate);
+                return selectedDateObj.getFullYear();
+            },
+
+            /**
              * Метод возвращает список месяцев
              *
              * @method
-             * @name jQuery.fn.masks#getMothsList
+             * @name jQuery.fn.calendar#getMothsList
              * @returns {undefined}
              */
             getMothsList: function () {
@@ -93,14 +205,82 @@ define([
             },
 
             /**
+             * Метод возвращает установленный месяц
+             *
+             * @method
+             * @name jQuery.fn.calendar#getSelectedMonth
+             * @returns {Number}
+             */
+            getSelectedMonth: function () {
+                var options = this.getOptions(),
+                    selectedDate = options.selectedDate,
+                    selectedDateObj,
+                    today;
+
+                if (_.isUndefined(selectedDate)) {
+                    today = new Date();
+                    return today.getMonth() + 1;
+                }
+
+                selectedDateObj = new Date(selectedDate);
+                return selectedDateObj.getMonth() + 1;
+            },
+
+            /**
              * Метод возвращает список дат
              *
              * @method
-             * @name jQuery.fn.masks#getDatesList
+             * @name jQuery.fn.calendar#getDatesList
              * @returns {undefined}
              */
             getDatesList: function () {
 
+            },
+
+            /**
+             * Метод возвращает установленный день
+             *
+             * @method
+             * @name jQuery.fn.calendar#getSelectedDate
+             * @returns {Number}
+             */
+            getSelectedDate: function () {
+                var options = this.getOptions(),
+                    selectedDate = options.selectedDate,
+                    selectedDateObj,
+                    today;
+
+                if (_.isUndefined(selectedDate)) {
+                    today = new Date();
+                    return today.getDate();
+                }
+
+                selectedDateObj = new Date(selectedDate);
+                return selectedDateObj.getDate();
+            },
+
+            /**
+             * Метод устанавливает дату календаря
+             *
+             * @method
+             * @name jQuery.fn.calendar#setDate
+             * @param {String} selectedDate timestamp текущей даты
+             * @returns {undefined}
+             */
+            setDate: function (selectedDate) {
+                this.setOptions({selectedDate: selectedDate});
+            },
+
+            /**
+             * Метод возвращает timestamp выставленной даты
+             *
+             * @method
+             * @name jQuery.fn.calendar#setDate
+             * @returns {String}
+             */
+            getDate: function () {
+                var options = this.getOptions();
+                return options.selectedDate;
             }
         };
 
