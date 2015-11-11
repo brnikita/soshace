bbda9ebcada17ba7629c110208f1872c1d089a98
Vlad Nikitin <nikitin_vi@soshace.com>
2015-11-11 12:55:18 +0300

    add middleware to parse encoded POST data

--- soshace/app.js
+++ soshace/app.js
@@ -57,6 +57,9 @@ var Blog = Class.extend({
      */
     configure: function () {
         App.use(bodyParser.json());
+        App.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+            extended: true
+        }));
         App.use(cookieParser());
         App.use(methodOverride());
         App.enable('view cache');
